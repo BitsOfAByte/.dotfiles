1002a2b5e9c203ffd2a9263742fd5ff29978ddec
Harmony <19539165+BitsOfAByte@users.noreply.github.com>
2022-04-11 19:14:53 +0100

    nullify output

--- programs/3-poetry.sh
+++ programs/3-poetry.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-curl -sSL https://install.python-poetry.org | python3 -
+curl -sSL https://install.python-poetry.org | python3 - > /dev/null
 poetry config virtualenvs.in-project true
