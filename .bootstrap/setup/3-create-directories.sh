9d8840b1197294bc958c1f2bef629ba9aad592b7
Harmony <19539165+BitsOfAByte@users.noreply.github.com>
2022-05-24 19:36:21 +0100

    add .gnupg fixes

--- .bootstrap/setup/3-create-directories.sh
+++ .bootstrap/setup/3-create-directories.sh
@@ -2,10 +2,17 @@
 
 echo "    [Setup/Directories] Creating directories"
 
+# Create key directories 
+
 # Create Development Directories
 mkdir -p $HOME/Dev/{langs,tools}
-mkdir -p $HOME/Dev/langs/{node,go,python,misc}
-mkdir -p $HOME/Dev/tools/
+mkdir -p $HOME/Dev/langs/{node,go,python,rust,misc}
+mkdir -p $HOME/Dev/langs/go/{bin,pkg,src} # For Go
 
-# Go
-mkdir -p $HOME/Dev/langs/go/{bin,pkg,src}
+# Make GPG Directory
+mkdir -p ~/.gnupg
+
+# Fix its permissions if they're broken.
+chown -R $(whoami) ~/.gnupg/
+chmod 600 ~/.gnupg/*
+chmod 700 ~/.gnupg
